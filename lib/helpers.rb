be3f8c6a07790db055044b6ef3980567d86b12d0
Kyle Mensing <kyle@kylemensing.com>
2020-03-26 14:41:28 -0400

    Delete helpers.rb

--- lib/helpers.rb
+++
@@ -1,9 +0,0 @@
-use_helper Nanoc::Helpers::Blogging
-use_helper Nanoc::Helpers::Capturing
-
-def publish_time_tag(date= nil)
-  date ||= @item[:created_at]
-  <<~HTML
-  <time datetime="#{date}">#{date.strftime("%B, %-d %Y")}</time>
-  HTML
-end
